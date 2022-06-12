9a23f59131e6e9db14ce0d5e707e5dbda3588b7d
Yaroslav Shergin <yaroslav.shergin@gmail.com>
2022-06-12 21:17:24 +0300

    new

--- react/lists/src/Pagination.jsx
+++ react/lists/src/Pagination.jsx
@@ -6,17 +6,17 @@ const Pagination = props => {
   const isPrevPageAvailable = currentPage === 1;
   const isNextPageAvailable = currentPage === Math.ceil(totalItems / itemsPerPage);
 
-  const showLeftArrow = () => (!isPrevPageAvailable ? '←' : '');
-  const showRightArrow = () => (!isNextPageAvailable ? '→' : '');
+  const hideLeftArrow = () => (isPrevPageAvailable ? '' : '←');
+  const hideRightArrow = () => (isNextPageAvailable ? '' : '→');
 
   return (
     <div className="pagination">
       <button className="btn" onClick={goPrev} disabled={isPrevPageAvailable}>
-        {showLeftArrow()}
+        {hideLeftArrow()}
       </button>
       <span className="pagination__page">{currentPage}</span>
       <button className="btn" onClick={goNext} disabled={isNextPageAvailable}>
-        {showRightArrow()}
+        {hideRightArrow()}
       </button>
     </div>
   );
